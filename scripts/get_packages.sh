2c74669bf77c6711302f6106d0b1af70714e7948
brahle <brahle@gmail.com>
2011-02-20 02:44:53 +0100

    Applying AGPLv3 License
    Summary: I finally came around to adding the license to the project. The master
    branch sholud now be licensed under AGPLv3. Since this is the first project I am
    licensing, please verify everthing is all right.
    
    Tags: licence, administration
    
    Reviewers: agrbin, arakipovic, canadi, fpavetic, gasho, IP314, ivankovic, kfrane, prasko, tomo
    
    Groups: administration
    
    Blamed Revisions:
    
    Tickets:
    
    Skip Reviewboard: NO
    
    ReviewBoardID: 24

--- scripts/get_packages.sh
+++ scripts/get_packages.sh
@@ -1,4 +1,21 @@
-# Install required packages 
-sudo apt-get install gcc g++ python-django
-sudo apt-get install libpqxx-3.0 libpqxx3-dev
-sudo apt-get install postgresql
+#!/bin/bash
+# Copyright 2011 Bruno Rahle
+#
+# This file is part of Evaluator.
+#
+# Evaluator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Evaluator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with Evaluator.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+# Install required packages
+sudo apt-get install gcc g++ python2.7 git-all postgresql python-django
