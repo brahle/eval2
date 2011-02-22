fa71bbc86dc88b538dddaf9c143a0664cbd389ae
prasko <mosrecki@gmail.com>
2011-02-22 18:59:15 +0100

    Code style fixes: worker source code
    Summary: Fixed all the code style bugs in WorkerServer.cpp.
    
    Tags:
    
    Reviewers:
    
    CCs:
    
    Blamed Revisions:
    
    Tickets:

--- src/worker/WorkerServer.cpp
+++ src/worker/WorkerServer.cpp
@@ -14,84 +14,79 @@
 #include "queue/gen-cpp/QueueService.h"
 #include "worker/gen-cpp/WorkerService.h"
 
-using namespace apache::thrift;
-using namespace apache::thrift::protocol;
-using namespace apache::thrift::transport;
-using namespace apache::thrift::server;
-
 using boost::shared_ptr;
 
 namespace eval {
 
 class WorkerServiceHandler : virtual public WorkerServiceIf {
- 
  public:
- 
+
   WorkerServiceHandler() {}
 
   bool ping() {
-    // TODO implement
+    // TODO(prasko): implement
     return true;
   }
 
   void giveTask(const int taskId) {
-    // TODO implement work
-    usleep(5);    
+    // TODO(prasko): implement work
+    usleep(5);
   }
-
 };
 
-} // namespace
+}  // namespace
 
 void registerSelf(int id, int port) {
-  shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
-  shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-  shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+  shared_ptr<apache::thrift::transport::TTransport> socket(
+    new apache::thrift::transport::TSocket("localhost", 9090));
+
+  shared_ptr<apache::thrift::transport::TTransport> transport(
+    new apache::thrift::transport::TBufferedTransport(socket));
+
+  shared_ptr<apache::thrift::protocol::TProtocol> protocol(
+    new apache::thrift::protocol::TBinaryProtocol(transport));
+
   QueueServiceClient client(protocol);
 
   try {
     transport->open();
-    
+
     client.ping();
 
     client.registerWorker(id, "localhost", port);
-    
+
     transport->close();
-  } catch (TException &tx) {
+  } catch(const apache::thrift::TException &tx) {
     printf("ERROR: %s\n", tx.what());
   }
 }
 
 void startServer(int port) {
   shared_ptr<eval::WorkerServiceHandler> handler(
-     new eval::WorkerServiceHandler());
+    new eval::WorkerServiceHandler());
 
-  shared_ptr<TProcessor> processor(
+  shared_ptr<apache::thrift::TProcessor> processor(
     new WorkerServiceProcessor(handler));
 
-  shared_ptr<TServerTransport> serverTransport(
-    new TServerSocket(port));
+  shared_ptr<apache::thrift::server::TServerTransport> serverTransport(
+    new apache::thrift::transport::TServerSocket(port));
 
-  shared_ptr<TTransportFactory> transportFactory(
-    new TBufferedTransportFactory());
+  shared_ptr<apache::thrift::transport::TTransportFactory> transportFactory(
+    new apache::thrift::transport::TBufferedTransportFactory());
 
-  shared_ptr<TProtocolFactory> protocolFactory(
-    new TBinaryProtocolFactory());
+  shared_ptr<apache::thrift::protocol::TProtocolFactory> protocolFactory(
+    new apache::thrift::protocol::TBinaryProtocolFactory());
 
-  TSimpleServer server(
-		       processor, 
-		       serverTransport, 
-		       transportFactory, 
-		       protocolFactory
-		       );
-  
-  server.serve();  
+  apache::thrift::server::TSimpleServer server(
+    processor, serverTransport, transportFactory, protocolFactory);
+
+  server.serve();
 }
 
 int main(int argc, char **argv) {
   assert(argc == 3);
 
-  // TODO read configuration: queue's ip+port, worker's ip+port or id
+  // TODO(prasko): read configuration: queue's ip+port, worker's ip+port or id
 
   int id = atoi(argv[1]);
   int port = atoi(argv[2]);
