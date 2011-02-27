765c12dd19e8c506cee9fd97505c0a1a2435f109
agrbin <anton.grbin@gmail.com>
2011-02-27 16:46:06 +0100

    Init_db changed
    Summary: -f changed to pipe
    
    Tags:
    
    Reviewers:
    
    CCs:
    
    Blamed Revisions:
    
    Tickets:

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install postgresql libpqxx3-dev
 
-sudo -u postgres psql -f ../src/db/sql/create_db.sql
-sudo -u postgres psql -f ../src/db/sql/tuna.sql
-sudo -u postgres psql -f ../src/db/sql/tables.sql
+sudo -u postgres psql < ../src/db/sql/create_db.sql
+sudo -u postgres psql < ../src/db/sql/tuna.sql
+sudo -u postgres psql < ../src/db/sql/tables.sql
 
