6ffd7da81e143649e7ca2ddab92f27bd913c6a15
agrbin <anton.grbin@gmail.com>
2011-03-01 22:20:43 +0100

    Scripts moved
    Summary: create_db is now moved to Makefile in src/db
    
    Tags:
    
    Reviewers:
    
    CCs:
    
    Blamed Revisions:
    
    Tickets:

--- scripts/init_db.sh
+++
@@ -1,8 +0,0 @@
-#get psotgresql
-
-sudo apt-get install postgresql libpqxx3-dev
-
-sudo -u postgres psql < ../src/db/sql/create_db.sql
-sudo -u postgres psql < ../src/db/sql/tuna.sql
-sudo -u postgres psql < ../src/db/sql/tables.sql
-
